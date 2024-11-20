33d5c97640a7fe70eebed87b74a37f3af91468cc
msle237-lees <59542013+msle237-lees@users.noreply.github.com>
2024-11-20 15:23:49 -0500

    added validation to the train_model function

--- testing/AIPackageTesting/ObjectDetection/sub_modules/train.py
+++ testing/AIPackageTesting/ObjectDetection/sub_modules/train.py
@@ -1,6 +1,6 @@
 # train.py
 from ultralytics import YOLO
-from sub_modules.config import DATASET_PATH, EPOCHS, IMG_SIZE, BATCH_SIZE
+from sub_modules.config import DATASET_PATH, EPOCHS, IMG_SIZE, BATCH_SIZE, OUTPUT_PATH, MODEL_NAME
 
 def train_model(model):
     """
@@ -12,7 +12,11 @@ def train_model(model):
         epochs=EPOCHS,
         imgsz=IMG_SIZE,
         batch=BATCH_SIZE,
+        project=OUTPUT_PATH,  # Custom outputs folder
+        name=MODEL_NAME, 
     )
     print("Training completed!")
+    metrics = model.val()  # no arguments needed, dataset and settings remembered
+    print("Validation metrics:", metrics)
 
 print("Training module loaded!")
