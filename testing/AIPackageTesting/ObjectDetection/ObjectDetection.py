6405941e54e39135b5f254b3b0d4188c5d357a15
msle237-lees <59542013+msle237-lees@users.noreply.github.com>
2024-11-20 15:48:55 -0500

    added validation to the train_model function

--- testing/AIPackageTesting/ObjectDetection/ObjectDetection.py
+++ testing/AIPackageTesting/ObjectDetection/ObjectDetection.py
@@ -2,7 +2,10 @@
 from sub_modules.model_utils import load_model
 from sub_modules.train import train_model
 
+from datetime import datetime
+
 if __name__ == "__main__":
+    start_time = datetime.now()
     # Load or create model
     model = load_model()
     
@@ -11,3 +14,8 @@ if __name__ == "__main__":
         train_model(model)
     else:
         print("Model is already trained and loaded!")
+    
+    end_time = datetime.now()
+    print("Time Started:", start_time)
+    print("Time Ended:", end_time)
+    print("Total Time Elapsed:", end_time - start_time)
