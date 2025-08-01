93b957ea2f5251afa5af5060ef421ac7d0b270bb
msle237-lees <msle237.lees@gmail.com>
2025-08-01 17:34:06 -0400

    trying to get imu working

--- src/modules/HISupport/IMU.py
+++ src/modules/HISupport/IMU.py
@@ -4,7 +4,7 @@ from adafruit_bno08x.i2c import BNO08X_I2C
 from adafruit_bno08x import BNO_REPORT_ACCELEROMETER
 
 i2c = busio.I2C(board.SCL, board.SDA)
-bno = BNO08X_I2C(i2c)
+bno = BNO08X_I2C(i2c, address=0x4B)
 bno.enable_feature(BNO_REPORT_ACCELEROMETER)
 
 while True:
