b7d9cdcc3d93ff106191aa669b1fb33af78f8eb0
msle237-lees <msle237.lees@gmail.com>
2025-08-13 23:49:15 -0700

    putting test data into database

--- libs/db_manager/__init__.py
+++ libs/db_manager/__init__.py
@@ -1,9 +1,14 @@
 from flask import Flask
 from flask_marshmallow import Marshmallow
 from .models import db
-from .routes.inputs_routes import inputs_bp
-from .routes.outputs_routes import outputs_bp
 from .routes.batteries_routes import batteries_bp
+from .routes.externals_routes import externals_bp
+from .routes.imu_routes import imu_bp
+from .routes.inputs_routes import inputs_bp
+from .routes.internals_routes import internals_bp
+from .routes.outputs_routes import outputs_bp
+from .routes.sonar_routes import sonar_bp
+
 
 ma = Marshmallow()
 
@@ -14,8 +19,13 @@ def create_app():
 
     db.init_app(app)
     ma.init_app(app)
+    app.register_blueprint(batteries_bp)
+    app.register_blueprint(externals_bp)
+    app.register_blueprint(imu_bp)
     app.register_blueprint(inputs_bp)
+    app.register_blueprint(internals_bp)
     app.register_blueprint(outputs_bp)
+    app.register_blueprint(sonar_bp)
 
     with app.app_context():
         db.create_all()
