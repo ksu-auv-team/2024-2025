409f54cb1c4b11848b723ac6c2ef1f68d03eb666
msle237-lees <msle237.lees@gmail.com>
2025-08-15 14:12:48 -0700

    please please please

--- libs/hardware_interface/hardware_interface.py
+++ libs/hardware_interface/hardware_interface.py
@@ -115,22 +115,49 @@ class HardwareInterface:
         self.qualify = qualify
 
     def qualification(self):
-        time.sleep(5)
-        breakCase = time.time() + 5*1
-        while(True):
+        starting_delay = 30
+        initial_movemnet = 60
+        down_hold = 10
+        second_movement = 60
+        up_movement = 60
+        time.sleep(starting_delay)
+        breakCase = time.time() + initial_movemnet
+        while True:
             motors: Dict[str, int] = {
                 "M1": int(255), "M2": int(255), "M3": int(0), "M4": int(255),
                 "M5": int(127), "M6": int(127), "M7": int(127), "M8": int(127)
             }
             self._MotorController(motors)
-            # motors: Dict[str, int] = {
-            #     "M1": int(127), "M2": int(127), "M3": int(127), "M4": int(127),
-            #     "M5": int(0), "M6": int(0), "M7": int(255), "M8": int(255)
-            # }
-            # self._MotorController(motors)
             if (time.time() > breakCase):
                 break
-        
+        breakCase = time.time() + down_hold
+        while True:
+            motors: Dict[str, int] = {
+                "M1": int(127), "M2": int(127), "M3": int(127), "M4": int(127),
+                "M5": int(127), "M6": int(127), "M7": int(127), "M8": int(127)
+            }
+            self._MotorController(motors)
+            if time.time() > down_hold:
+                break
+        breakCase = time.time() + second_movement
+        while True:
+            motors: Dict[str, int] = {
+                "M1": int(255), "M2": int(255), "M3": int(0), "M4": int(255),
+                "M5": int(127), "M6": int(127), "M7": int(127), "M8": int(127)
+            }
+            self._MotorController(motors)
+            if time.time() > breakCase:
+                break
+        breakCase = time.time() + up_movement
+        while True:
+            motors: Dict[str, int] = {
+                "M1": int(127), "M2": int(127), "M3": int(127), "M4": int(127),
+                "M5": int(0), "M6": int(0), "M7": int(255), "M8": int(255)
+            }
+            self._MotorController(motors)
+            if time.time() > breakCase:
+                break
+
     # ------------------------------- Small helpers -------------------------------
 
     @staticmethod
