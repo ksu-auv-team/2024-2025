6dea3fe2d51dd14992e69624485df04479c17fa9
msle237-lees <msle237.lees@gmail.com>
2025-08-15 14:22:36 -0700

    please please please

--- libs/hardware_interface/hardware_interface.py
+++ libs/hardware_interface/hardware_interface.py
@@ -116,7 +116,7 @@ class HardwareInterface:
 
     def qualification(self):
         starting_delay = 30
-        initial_movemnet = 8
+        initial_movemnet = 6
         down_hold = 10
         second_movement = 60
         up_movement = 60
