fa1a079a2eb38621fe3a6479b6882e47b15e57d7
msle237-lees <msle237.lees@gmail.com>
2025-08-15 14:40:35 -0700

    please please please

--- libs/hardware_interface/hardware_interface.py
+++ libs/hardware_interface/hardware_interface.py
@@ -131,14 +131,14 @@ class HardwareInterface:
         while True:
             motors: Dict[str, int] = {
                 "M1": int(255), "M2": int(255), "M3": int(0), "M4": int(255),
-                "M5": int(127), "M6": int(127), "M7": int(127), "M8": int(127)
+                "M5": int(0), "M6": int(0), "M7": int(127), "M8": int(127)
             }
             self._MotorController(motors)
             if (time.time() > breakCase):
                 break
         motors: Dict[str, int] = {
             "M1": int(127), "M2": int(127), "M3": int(127), "M4": int(127),
-            "M5": int(127), "M6": int(127), "M7": int(127), "M8": int(127)
+            "M5": int(127), "M6": int(127), "M7": int(255), "M8": int(0)
         }
         self._MotorController(motors)
         time.sleep(2)
@@ -151,6 +151,12 @@ class HardwareInterface:
             self._MotorController(motors)
             if (time.time() > breakCase):
                 break
+        motors: Dict[str, int] = {
+            "M1": int(127), "M2": int(127), "M3": int(127), "M4": int(127),
+            "M5": int(127), "M6": int(127), "M7": int(127), "M8": int(127)
+        }
+        self._MotorController(motors)
+        time.sleep(2)
         # breakCase = time.time() + down_hold
         # while True:
         #     motors: Dict[str, int] = {
