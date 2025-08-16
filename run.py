35a171b7a10ff3b152281bd86b6bcea5d1c37337
Ryan Slovensky <ryaneslovensky@gmail.com>
2025-08-16 12:27:37 -0400

    Changed timing

--- run.py
+++ run.py
@@ -173,7 +173,7 @@ def main():
         threads.append(t_err)
 
         if proc_name == "hardware_interface":
-            time.sleep(60)
+            time.sleep(2.5)
         else:
             time.sleep(2.5)
     
