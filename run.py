4e9be649f71a792e1bbfdecd4e5d723821d043c2
msle237-lees <msle237.lees@gmail.com>
2025-08-15 23:24:47 -0700

    pplease

--- run.py
+++ run.py
@@ -61,9 +61,8 @@ def main():
 
     real_world_processes = [
         ["python", "-m", "libs.db_manager.run"],
-        # ["python", "-m", "libs.data_visualizer.run"],
-        ["python", "-m", "libs.hardware_interface.run"],
-        ["python", "-m", "libs.movement_package.run"]
+        ["python", "-m", "libs.movement_package.run"],
+        ["python", "-m", "libs.hardware_interface.run"]
     ]
 
     to_qualify_processes = [
