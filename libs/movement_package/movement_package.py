462d73c9d604b3219439796ce7ec139a7f47191e
msle237.lees <msle237.lees@gmail.com>
2026-04-22 18:18:01 -0400

    repairs to fix i2c part 1

--- libs/movement_package/movement_package.py
+++ libs/movement_package/movement_package.py
@@ -114,13 +114,13 @@ class MovementPackage:
             self.logger.warning("No parsed inputs available.")
             return self.combined_output
 
-        if bool(self.parsed_inputs["Arm"]):
+        if bool(self.parsed_inputs["arm"]):
             # Compute PID outputs in normalized space
             self.PID.update_motors(
-                x=float(self.parsed_inputs["X"]),
-                y=float(self.parsed_inputs["Y"]),
-                z=float(self.parsed_inputs["Z"]),
-                yaw=float(self.parsed_inputs["Yaw"])
+                x=float(self.parsed_inputs["x"]),
+                y=float(self.parsed_inputs["y"]),
+                z=float(self.parsed_inputs["z"]),
+                yaw=float(self.parsed_inputs["yaw"])
             )
 
             motors = self._pid_flat()
